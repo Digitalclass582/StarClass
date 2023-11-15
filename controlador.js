e37c3578735c7b2f34aadaa2a6bc53ba1924a1e2
julianaalvarezbot@gmail.com <julianaalvarezbot@gmail.com>
2023-11-15 14:08:49 -0500

    se crea controlador

---
+++ controlador.js
@@ -0,0 +1,17 @@
+let botonFormulario=document.getElementById("botonlogin")
+botonFormulario.addEventListener("click",function(){
+    let usuario=document.getElementById("correo")
+    let contrasena=document.getElementById("password")
+
+    setTimeout(function(){ window.location.href="./index.html"
+},1000)
+Swal.fire({
+    title: "BIENVENIDO " +usuario.value,
+    icon: "success"
+  })
+})
+let botonGoogle=document.getElementById("botongoogle")
+botonGoogle.addEventListener("click",function(){
+    setTimeout(function(){ window.location.href="https://www.google.com/intl/es-419/gmail/about/"
+},1000)
+})
